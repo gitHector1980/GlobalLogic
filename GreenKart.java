60654c7fbbe2de87df16e7dade94771e01f5c389
Hector Ochoa <vedder_30@hotmail.com>
2026-02-04 12:59:02 -0600

    Cambios en la clase de: GreenKart identacion

--- GreenKart.java
+++ GreenKart.java
@@ -1,4 +1,5 @@
 package com.seleniumpractice.tutorial;
+
 import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,10 +15,10 @@ public class GreenKart {
 
 	private WebDriver driver;
 	private WebDriverWait wait;
-	
+
 	@Test
 	public void addToGreenKart1() {
-		// Elementos de la pagina de GreenKart							
+		// Elementos de la pagina de GreenKart
 		By productBrocolli = By.xpath("//*[@id='root']/div/div[1]/div/div[1]/div[3]/button");
 		wait.until(ExpectedConditions.visibilityOfElementLocated(productBrocolli));
 		By productCauliflower = By.xpath("//*[@id='root']/div/div[1]/div/div[2]/div[3]/button");
@@ -27,38 +28,36 @@ public class GreenKart {
 		By productTomato = By.xpath("//*[@id='root']/div/div[1]/div/div[6]/div[3]/button");
 		By productBeans = By.xpath("//*[@id='root']/div/div[1]/div/div[7]/div[3]/button");
 		By productBrinjal = By.xpath("//*[@id='root']/div/div[1]/div/div[8]/div[3]/button");
-		//Capturar los elementos deseados en un arreglo
-		By[] products = {productBrocolli, productCauliflower, productCucumber, productBeetroot, productCarrot, productTomato, productBeans, productBrinjal};
-		//Iterar el arreglo
+		// Capturar los elementos deseados en un arreglo
+		By[] products = { productBrocolli, productCauliflower, productCucumber, productBeetroot, productCarrot,
+				productTomato, productBeans, productBrinjal };
+		// Iterar el arreglo
 		for (By product : products) {
-			//Condicional para cuando los elelentos dentro del arreglo no estén desplegados
+			// Condicional para cuando los elelentos dentro del arreglo no estén desplegados
 			if (!driver.findElement(product).isDisplayed()) {
 				System.out.println("Alguno de los productos No están visibles");
 				throw new AssertionError("Product not visible: " + product);
-			//Si están desplegados hacemos click a los elelentos	
-			}else {			
-			driver.findElement(product).click();
+				// Si están desplegados hacemos click a los elelentos
+			} else {
+				driver.findElement(product).click();
 			}
 		}
-		//Imprimimos por consola un mensaje
+		// Imprimimos por consola un mensaje
 		System.out.println("Todos los productos estan visibles");
-		
+
 		By cartCounter = By.xpath("//*[@id='root']/div/header/div/div[3]/div[1]/table/tbody/tr[1]/td[3]/strong");
 		wait.until(ExpectedConditions.presenceOfElementLocated(cartCounter));
 		String verifyElement = driver.findElement(cartCounter).getText();
-				
-		if("8".equals(verifyElement)) {
+
+		if ("8".equals(verifyElement)) {
 			System.out.println("Son 8 elementos");
 		}
-		
+
 		Assert.assertEquals("8", verifyElement);
 		System.out.println(verifyElement);
-		
-		
-		
-	
+
 	}
-	
+
 	// Set up the property and the browser to open the web page
 	@BeforeEach
 	public void setUp() {
@@ -71,11 +70,11 @@ public class GreenKart {
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
 	}
+
 	@AfterEach
 	public void tearDown() {
 		driver.quit();
-		
+
 	}
-	
 
 }
